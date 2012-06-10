f4fca30ab7cb3b45dd987089631cecf8b980d44b
Bart <none@none>
2012-06-10 16:54:04 +0200

    Added admin view for real transactions.

---
+++ transactionreal/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from transactionreal.models import TransactionReal
+
+class TransactionRealAdmin(admin.ModelAdmin):
+  fieldsets = [
+    (None, {'fields': ['amount']}),
+    (None, {'fields': ['sender']}),
+    (None, {'fields': ['receiver']}),
+    (None, {'fields': ['comment']}),
+    (None, {'fields': ['groupAccount']}),
+    (None, {'fields': ['date']}), ]
+  list_display = ('amount', 'sender', 'receiver', 'groupAccount', 'comment', 'date')
+  list_filter = ['date']
+  search_fields = ['what']
+  date_hierarchy = 'date'
+
+admin.site.register(TransactionReal, TransactionRealAdmin)
+
+#admin.site.register(Account, AccountAdmin)
