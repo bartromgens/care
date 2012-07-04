8c7970e768a1fa1f643366e39b2927d127d6f7cc
Bart <none@none>
2012-07-04 22:32:07 +0200

    Added ssh comments.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
+# ssh bartromgens@bartromgens.webfactional.com
+# ~/webapps/care/apache2/bin/restart
 scp -r ../. bartromgens@bartromgens.webfactional.com:~/webapps/care/care/
 scp ../webfaction/*.py bartromgens@bartromgens.webfactional.com:~/webapps/care/care/base/
 scp ../static/style.css bartromgens@bartromgens.webfactional.com:~/webapps/static/
