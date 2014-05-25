29c264f9a46304e49c788b528e8a264148c3e6a1
Bart Romgens <bart.romgens@gmail.com>
2014-05-25 20:45:48 +0200

    Minor improvement in labels of invite form.

--- groupaccountinvite/forms.py
+++ groupaccountinvite/forms.py
@@ -15,15 +15,15 @@ class NewInviteForm(forms.ModelForm):
     userProfile = UserProfile.objects.get(user=user);
     super(NewInviteForm, self).__init__(*args, **kwargs)
     
-    self.fields['inviter'] = forms.ModelChoiceField(UserProfile.objects.filter(id=userProfile.id), empty_label=None, label='User')
+    self.fields['inviter'] = forms.ModelChoiceField(UserProfile.objects.filter(id=userProfile.id), empty_label=None, label='You')
     self.fields['inviter'].initial = userProfile
     self.fields['inviter'].widget.attrs['readonly'] = True
     
     logger.debug(userProfile.groupAccounts)
         
-    self.fields['invitee'] = forms.ModelChoiceField(queryset=UserProfile.objects.all(), label='Invites')
+    self.fields['invitee'] = forms.ModelChoiceField(queryset=UserProfile.objects.all(), label='invite')
     
-    self.fields['groupAccount'] = forms.ModelChoiceField(queryset=userProfile.groupAccounts, empty_label=None, label='To group')
+    self.fields['groupAccount'] = forms.ModelChoiceField(queryset=userProfile.groupAccounts, empty_label=None, label='to group')
 
     self.fields['isAccepted'] = forms.BooleanField(widget=forms.HiddenInput, required=False)
     self.fields['isDeclined'] = forms.BooleanField(widget=forms.HiddenInput, required=False)
