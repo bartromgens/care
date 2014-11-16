e4effd8fe3a8da8b7baa9c83b1b40e14f43a34a4
Bart Romgens <bart.romgens@gmail.com>
2014-11-16 23:38:16 +0100

    Renamed some variables.

--- base/mailnotification.py
+++ base/mailnotification.py
@@ -4,57 +4,57 @@ from transactionreal.models import TransactionReal
 from datetime import timedelta
 
 def create_transaction_history_table_html(userprofile, date_start, date_end):
-    transactionsAll = Transaction.get_transactions_sorted_by_last_modified(userprofile.id)
+    transactions_all = Transaction.get_transactions_sorted_by_last_modified(userprofile.id)
 
-    transactionsInRange = []
-    for transaction in transactionsAll:
+    transactions_in_timerange = []
+    for transaction in transactions_all:
         if (transaction.lastModified.date() > date_start) and (transaction.lastModified.date() < date_end + timedelta(1)):
-            transactionsInRange.append(transaction)
+            transactions_in_timerange.append(transaction)
 
-    if not transactionsInRange:
+    if not transactions_in_timerange:
         return 'No shares in date range.'
-    transactionTableHtml = '<table>'
-    transactionTableHtml += '<tr align=\'left\'>'
-    transactionTableHtml += '<th><b>&#8364;/pp</b></th>'
-    transactionTableHtml += '<th><b>&#8364;</b></th>'
-    transactionTableHtml += '<th><b>What</b></th>'
-    transactionTableHtml += '<th><b>Who</b></th>'
-    transactionTableHtml += '<th><b>Date</b></th>'
-    for transaction in transactionsInRange:
-        transactionTableHtml += '<tr style="font-size: 12px">'
-        transactionTableHtml += '<td>&#8364;' + transaction.amountPerPerson + '</td>'
-        transactionTableHtml += '<td>&#8364;' + '%.2f' % float(transaction.amount) + '</td>'
-        transactionTableHtml += '<td>' + transaction.what + '</td>'
-        transactionTableHtml += '<td>' + transaction.buyer.displayname + '</td>'
-        transactionTableHtml += '<td>' + transaction.date.strftime('%d %b') + '</td>'
-        transactionTableHtml += '</tr>'
+    transaction_table = '<table>'
+    transaction_table += '<tr align=\'left\'>'
+    transaction_table += '<th><b>&#8364;/pp</b></th>'
+    transaction_table += '<th><b>&#8364;</b></th>'
+    transaction_table += '<th><b>What</b></th>'
+    transaction_table += '<th><b>Who</b></th>'
+    transaction_table += '<th><b>Date</b></th>'
+    for transaction in transactions_in_timerange:
+        transaction_table += '<tr style="font-size: 12px">'
+        transaction_table += '<td>&#8364;' + transaction.amountPerPerson + '</td>'
+        transaction_table += '<td>&#8364;' + '%.2f' % float(transaction.amount) + '</td>'
+        transaction_table += '<td>' + transaction.what + '</td>'
+        transaction_table += '<td>' + transaction.buyer.displayname + '</td>'
+        transaction_table += '<td>' + transaction.date.strftime('%d %b') + '</td>'
+        transaction_table += '</tr>'
 
-    transactionTableHtml += '</table>'
-    return transactionTableHtml
+    transaction_table += '</table>'
+    return transaction_table
 
 
 def create_transaction_real_history_table_html(userprofile, date_start, date_end):
-    transactionsRealAll = TransactionReal.get_transactions_real_sorted_by_last_modified(userprofile.id)
-    transactionsInRange = []
-    for transaction in transactionsRealAll:
+    transactionreal_all = TransactionReal.get_transactions_real_sorted_by_last_modified(userprofile.id)
+    transactions_in_timerange = []
+    for transaction in transactionreal_all:
         if transaction.lastModified.date() > date_start and transaction.lastModified.date() < date_end + timedelta(1):
-            transactionsInRange.append(transaction)
+            transactions_in_timerange.append(transaction)
 
-    if not transactionsInRange:
+    if not transactions_in_timerange:
         return 'No transactions in date range.'
-    transactionTableHtml = '<table style="font-size: 12px">'
-    transactionTableHtml += '<tr align=\'left\'>'
-    transactionTableHtml += '<th><b>&#8364;</b></th>'
-    transactionTableHtml += '<th><b>From</b></th>'
-    transactionTableHtml += '<th><b>To</b></th>'
-    transactionTableHtml += '<th><b>Date</b></th>'
-    for transaction in transactionsInRange:
-        transactionTableHtml += '<tr>'
-        transactionTableHtml += '<td>&#8364;' + '%.2f' % float(transaction.amount) + '</td>'
-        transactionTableHtml += '<td>' + transaction.sender.displayname + '</td>'
-        transactionTableHtml += '<td>' + transaction.receiver.displayname + '</td>'
-        transactionTableHtml += '<td>' + transaction.date.strftime('%d %b') + '</td>'
-        transactionTableHtml += '</tr>'
+    transaction_table = '<table style="font-size: 12px">'
+    transaction_table += '<tr align=\'left\'>'
+    transaction_table += '<th><b>&#8364;</b></th>'
+    transaction_table += '<th><b>From</b></th>'
+    transaction_table += '<th><b>To</b></th>'
+    transaction_table += '<th><b>Date</b></th>'
+    for transaction in transactions_in_timerange:
+        transaction_table += '<tr>'
+        transaction_table += '<td>&#8364;' + '%.2f' % float(transaction.amount) + '</td>'
+        transaction_table += '<td>' + transaction.sender.displayname + '</td>'
+        transaction_table += '<td>' + transaction.receiver.displayname + '</td>'
+        transaction_table += '<td>' + transaction.date.strftime('%d %b') + '</td>'
+        transaction_table += '</tr>'
 
-    transactionTableHtml += '</table>'
-    return transactionTableHtml
+    transaction_table += '</table>'
+    return transaction_table
