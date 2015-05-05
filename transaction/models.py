b44d733e4c8a9bc898c9901a9b15cc54a7c2dbcd
Bart Römgens <bart.romgens@gmail.com>
2015-05-05 15:01:19 +0200

    Addition to d347a46c710bde94b6f4e65e7a2f2606d73fe254

--- transaction/models.py
+++ transaction/models.py
@@ -1,16 +1,13 @@
-from groupaccount.models import GroupAccount
-from userprofile.models import UserProfile
-
-from django.db import models
-
 from datetime import datetime
 from itertools import chain
 
-# users = User.objects.filter(groups__name='monkeys')
+from django.db import models
 
-class Modification(models.Model):
-    user = models.ForeignKey(UserProfile, blank=True)
-    date = models.DateTimeField(default=datetime.now, editable=True, blank=True)
+from groupaccount.models import GroupAccount
+from userprofile.models import UserProfile
+from transactionreal.models import TransactionReal
+
+# users = User.objects.filter(groups__name='monkeys')
 
 
 class Transaction(models.Model):
@@ -20,12 +17,12 @@ class Transaction(models.Model):
     consumers = models.ManyToManyField(UserProfile, related_name='consumers')
     group_account = models.ForeignKey(GroupAccount)
     comment = models.CharField(max_length=200, blank=True)
-    modifications = models.ManyToManyField(Modification, blank=True)
     date = models.DateTimeField(default=datetime.now, editable=True, blank=True)
 
     def get_datetime_last_modified(self):
-        if self.modifications.all():
-            modification = self.modifications.latest('date')
+        modifications = Modification.objects.filter(transaction=self)
+        if modifications.exists():
+            modification = modifications.latest('date')
             return modification.date
         else:
             return self.date
@@ -39,8 +36,8 @@ class Transaction(models.Model):
         return transactions
 
     @staticmethod
-    def get_consumer_transactions(consumerId):
-        transactions = Transaction.objects.filter(consumers__id=consumerId).order_by("-date")
+    def get_consumer_transactions(consumer_id):
+        transactions = Transaction.objects.filter(consumers__id=consumer_id).order_by("-date")
         for transaction in transactions:
             transaction.amountPerPerson = '%.2f' % (-1*float(transaction.amount)/transaction.consumers.count())
             transaction.amountPerPersonFloat = (-1*float(transaction.amount)/transaction.consumers.count())
@@ -48,12 +45,19 @@ class Transaction(models.Model):
 
     @staticmethod
     def get_transactions_sorted_by_last_modified(userProfileId):
-        buyerTransactions = Transaction.get_buyer_transactions(userProfileId)
-        consumerTransactions = Transaction.get_consumer_transactions(userProfileId)
-        transactionsAll = list(chain(buyerTransactions, consumerTransactions))
-        for transaction in transactionsAll:
-            transaction.lastModified = transaction.get_datetime_last_modified();
-        return sorted(transactionsAll, key=lambda instance: instance.lastModified, reverse=True)
+        buyer_transactions = Transaction.get_buyer_transactions(userProfileId)
+        consumer_transactions = Transaction.get_consumer_transactions(userProfileId)
+        transactions_all = list(chain(buyer_transactions, consumer_transactions))
+        for transaction in transactions_all:
+            transaction.lastModified = transaction.get_datetime_last_modified()
+        return sorted(transactions_all, key=lambda instance: instance.lastModified, reverse=True)
 
     def __str__(self):
         return self.what
+
+
+class Modification(models.Model):
+    user = models.ForeignKey(UserProfile, blank=True)
+    date = models.DateTimeField(default=datetime.now, editable=True, blank=True)
+    transaction = models.ForeignKey(Transaction, blank=True, null=True)
+    transaction_real = models.ForeignKey(TransactionReal, blank=True, null=True)
