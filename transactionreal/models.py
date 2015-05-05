d175904f65d6f398403cd6ff4dfd5bab7209b362
Bart Römgens <bart.romgens@gmail.com>
2015-05-05 15:00:48 +0200

    Remove ManyToManyField in transaction and transactionreal models
    No need for this relation, can simply be a ForeignKey.

--- transactionreal/models.py
+++ transactionreal/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 from groupaccount.models import GroupAccount
 from userprofile.models import UserProfile
-from transaction.models import Modification
 
 
 class TransactionReal(models.Model):
@@ -14,40 +13,41 @@ class TransactionReal(models.Model):
     receiver = models.ForeignKey(UserProfile, related_name='receiver')
     comment = models.CharField(max_length=200)
     group_account = models.ForeignKey(GroupAccount)
-    modifications = models.ManyToManyField(Modification, blank=True)
     date = models.DateTimeField(default=datetime.now, editable=True, blank=True)
 
     def get_datetime_last_modified(self):
-        if self.modifications.all():
-            modification = self.modifications.latest('date')
+        from transaction.models import Modification
+        modifications = Modification.objects.filter(transaction_real=self)
+        if modifications.exists():
+            modification = modifications.latest('date')
             return modification.date
         else:
             return self.date
 
     @staticmethod
-    def get_transactions_real_sent(senderId):
-        transactions = TransactionReal.objects.filter(sender__id=senderId).order_by("-date")
+    def get_transactions_real_sent(sender_id):
+        transactions = TransactionReal.objects.filter(sender__id=sender_id).order_by("-date")
         for transaction in transactions:
             transaction.amountPerPerson = '%.2f' % transaction.amount
             transaction.amountPerPersonFloat = float(transaction.amount)
         return transactions
 
     @staticmethod
-    def get_transactions_real_received(receiverId):
-        transactions = TransactionReal.objects.filter(receiver__id=receiverId).order_by("-date")
+    def get_transactions_real_received(receiver_id):
+        transactions = TransactionReal.objects.filter(receiver__id=receiver_id).order_by("-date")
         for transaction in transactions:
             transaction.amountPerPerson = '%.2f' % transaction.amount
             transaction.amountPerPersonFloat = float(transaction.amount)
         return transactions
 
     @staticmethod
-    def get_transactions_real_sorted_by_last_modified(userProfileId):
-        sentTransactions = TransactionReal.get_transactions_real_sent(userProfileId)
-        receivedTransactions = TransactionReal.get_transactions_real_received(userProfileId)
-        transactionsRealAll = list(chain(sentTransactions, receivedTransactions))
-        for transaction in transactionsRealAll:
+    def get_transactions_real_sorted_by_last_modified(userprofile_id):
+        sent_transactions = TransactionReal.get_transactions_real_sent(userprofile_id)
+        received_transactions = TransactionReal.get_transactions_real_received(userprofile_id)
+        transactions_real_all = list(chain(sent_transactions, received_transactions))
+        for transaction in transactions_real_all:
             transaction.lastModified = transaction.get_datetime_last_modified();
-        return sorted(transactionsRealAll, key=lambda instance: instance.lastModified, reverse=True)
+        return sorted(transactions_real_all, key=lambda instance: instance.lastModified, reverse=True)
 
     def __str__(self):
         return self.comment
