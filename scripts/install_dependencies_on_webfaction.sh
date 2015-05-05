a383cec96d595af4cb9f6be712432a5e3ca2cf3b
Bart Römgens <bart.romgens@gmail.com>
2015-05-05 12:40:37 +0200

    Add script to install dependencies on webfaction which does not use pip

---
+++ scripts/install_dependencies_on_webfaction.sh
@@ -0,0 +1,1 @@
+PYTHONPATH=$HOME/webapps/care_0_3/lib/python3.4 easy_install-3.4 --install-dir=$HOME/webapps/care_0_3/lib/python3.4 --script-dir=$HOME/webapps/care_0_3/bin django-bootstrap3 django-bootstrap3-datetimepicker django-common-helpers django-cron django-registration-redux
