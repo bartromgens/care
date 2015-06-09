3ba3cf3e61bef046f186fffd4b9aff9f4770a0a9
Bart Römgens <bart.romgens@gmail.com>
2015-06-09 21:58:49 +0200

    Add python version in migration script for deployment server env

--- scripts/migrate_initial_django18.sh
+++ scripts/migrate_initial_django18.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env bash
+
 cd ..
 git checkout v0.2.2 
-python manage.py migrate --fake-initial 
+python3.4 manage.py migrate --fake-initial
 git checkout v0.2.3 
-python manage.py migrate
+python3.4 manage.py migrate
 git checkout v0.2.4 
-python manage.py migrate
+python3.4 manage.py migrate
 git checkout v0.2.5 
-python manage.py migrate
+python3.4 manage.py migrate
