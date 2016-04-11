23ed1544d710b7942c014d159921afe69b2e84ff
Bart Römgens <bart.romgens@gmail.com>
2016-04-11 15:18:45 +0200

    Refactor transaction.forms
    - Remove duplicate code
    - Improve code style and formatting

--- care/transaction/forms.py
+++ care/transaction/forms.py
@@ -13,41 +13,15 @@ from care.userprofile.models import UserProfile
 logger = logging.getLogger(__name__)
 
 
-class NewTransactionForm(forms.ModelForm):
-    ## TODO BR: remove duplicate code with EditTransactionForm
-    def __init__(self, group_account_id, user, *args, **kwargs):
+class TransactionForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
         self.fields['is_shared_by_all'] = forms.BooleanField(label='Shared by all', required=False)
-        self.fields['consumers'] = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(), 
-                                                                  queryset=UserProfile.objects.filter(group_accounts=group_account_id),
-                                                                  label='Shared by some', required=False)
-
-        self.fields['buyer'] = forms.ModelChoiceField(queryset=UserProfile.objects.filter(group_accounts=group_account_id),
-                                                      empty_label=None)
-
-        self.fields['buyer'].initial = UserProfile.objects.get(user=user)
         self.fields['what'].label = 'What'
         self.fields['amount'].label = 'Cost (€)'
-        self.fields['group_account'] = forms.ModelChoiceField(queryset=UserProfile.objects.get(user=user).group_accounts,
-                                                              widget=forms.Select(attrs={"onChange":'form.submit()'}),
-                                                              empty_label=None,
-                                                              label='Group')
-        if GroupAccount.objects.filter(id=group_account_id).count():
-            self.fields['group_account'].initial = GroupAccount.objects.get(id=group_account_id)
-            
-        self.fields['date'] = forms.DateTimeField(widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}), 
-                                                  initial=datetime.now)
-
-        self.fields['modifications'] = forms.ModelMultipleChoiceField(queryset=Modification.objects.all(), 
-                                                                      required=False, 
-                                                                      widget=forms.MultipleHiddenInput())
-
-    def set_group_account(self, group_account):
-        self.fields['group_account'].initial = group_account
 
     def clean(self):
-        cleaned_data = super(NewTransactionForm, self).clean()
+        cleaned_data = super().clean()
         if not cleaned_data.get("consumers") and not cleaned_data.get("is_shared_by_all"):
             raise forms.ValidationError("Please select someone to share with.")
         return cleaned_data
@@ -57,38 +31,75 @@ class NewTransactionForm(forms.ModelForm):
         fields = '__all__'
 
 
-class EditTransactionForm(forms.ModelForm):
-    def __init__(self, transaction_id, user, *args, **kwargs):
+class NewTransactionForm(TransactionForm):
+    def __init__(self, group_account_id, user, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.fields['consumers'] = forms.ModelMultipleChoiceField(
+            widget=forms.CheckboxSelectMultiple(),
+            queryset=UserProfile.objects.filter(group_accounts=group_account_id),
+            label='Shared by some',
+            required=False
+        )
+
+        self.fields['buyer'] = forms.ModelChoiceField(
+            queryset=UserProfile.objects.filter(group_accounts=group_account_id),
+            empty_label=None
+        )
+        self.fields['buyer'].initial = UserProfile.objects.get(user=user)
+
+        self.fields['group_account'] = forms.ModelChoiceField(
+            queryset=UserProfile.objects.get(user=user).group_accounts,
+            widget=forms.Select(attrs={"onChange":'form.submit()'}),
+            empty_label=None,
+            label='Group'
+        )
+        if GroupAccount.objects.filter(id=group_account_id).count():
+            self.fields['group_account'].initial = GroupAccount.objects.get(id=group_account_id)
+            
+        self.fields['date'] = forms.DateTimeField(
+            widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}),
+            initial=datetime.now
+        )
+
+        self.fields['modifications'] = forms.ModelMultipleChoiceField(
+            queryset=Modification.objects.all(),
+            required=False,
+            widget=forms.MultipleHiddenInput()
+        )
+
+    def set_group_account(self, group_account):
+        self.fields['group_account'].initial = group_account
+
+
+class EditTransactionForm(TransactionForm):
+    def __init__(self, transaction_id, user, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         transaction = Transaction.objects.get(id=transaction_id)
 
-        self.fields['is_shared_by_all'] = forms.BooleanField(label='Shared by all', required=False)
-        self.fields['consumers'] = forms.ModelMultipleChoiceField(widget=forms.CheckboxSelectMultiple(),
-                                                                  queryset=UserProfile.objects.filter(group_accounts=transaction.group_account),
-                                                                  label='Shared by some', required=False)
-        self.fields['buyer'] = forms.ModelChoiceField(queryset=UserProfile.objects.filter(group_accounts=transaction.group_account),
-                                                      empty_label=None)
-        self.fields['what'].label = 'What'
-        self.fields['amount'].label = 'Cost (€)'
-        self.fields['group_account'] = forms.ModelChoiceField(queryset=GroupAccount.objects.filter(id=transaction.group_account.id),
-                                                             empty_label=None,
-                                                             label='Group')
+        self.fields['consumers'] = forms.ModelMultipleChoiceField(
+            widget=forms.CheckboxSelectMultiple(),
+            queryset=UserProfile.objects.filter(group_accounts=transaction.group_account),
+            label='Shared by some',
+            required=False
+        )
+
+        self.fields['buyer'] = forms.ModelChoiceField(
+            queryset=UserProfile.objects.filter(group_accounts=transaction.group_account),
+            empty_label=None
+        )
+
+        self.fields['group_account'] = forms.ModelChoiceField(
+            queryset=GroupAccount.objects.filter(id=transaction.group_account.id),
+            empty_label=None,
+            label='Group'
+        )
+
         self.fields['date'] = forms.DateTimeField(widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}))
         # self.fields['modifications'] = forms.ModelMultipleChoiceField(queryset=Modification.objects.all(),
         #                                                               required=False,
         #                                                               widget=forms.MultipleHiddenInput())
         self.fields['group_account'].widget.attrs['readonly'] = True
-
-    def clean(self):
-        cleaned_data = super(EditTransactionForm, self).clean()
-        if not cleaned_data.get("consumers") and not cleaned_data.get("is_shared_by_all"):
-            raise forms.ValidationError("Please select someone to share with.")
-        return cleaned_data
-
-    class Meta:
-        model = Transaction
-        fields = '__all__'
 
 
 class NewRealTransactionForm(forms.ModelForm):
@@ -97,21 +108,38 @@ class NewRealTransactionForm(forms.ModelForm):
 
         #  self.fields['sender'] = forms.ModelChoiceField(queryset=UserProfile.objects.get(user=user),
         #  widget = forms.HiddenInput, empty_label=None, label='From')
-        self.fields['sender'] = forms.ModelChoiceField(queryset=UserProfile.objects.filter(user=user),
-                                                       empty_label=None, label='From', widget=forms.HiddenInput())
+        self.fields['sender'] = forms.ModelChoiceField(
+            queryset=UserProfile.objects.filter(user=user),
+            empty_label=None,
+            label='From',
+            widget=forms.HiddenInput()
+        )
         self.fields['sender'].initial = UserProfile.objects.get(user=user)
-        self.fields['receiver'] = forms.ModelChoiceField(queryset=UserProfile.objects.filter(group_accounts=group_account_id),
-                                                         empty_label=None, label='To')
+        self.fields['receiver'] = forms.ModelChoiceField(
+            queryset=UserProfile.objects.filter(group_accounts=group_account_id),
+            empty_label=None,
+            label='To'
+        )
+
         self.fields['comment'] = forms.CharField(required=False)
         self.fields['amount'].label = '€'
-        self.fields['group_account'] = forms.ModelChoiceField(queryset=UserProfile.objects.get(user=user).group_accounts, widget=forms.Select(attrs={"onChange":'form.submit()'}), empty_label=None, label='Group')
+
+        self.fields['group_account'] = forms.ModelChoiceField(
+            queryset=UserProfile.objects.get(user=user).group_accounts,
+            widget=forms.Select(attrs={"onChange":'form.submit()'}),
+            empty_label=None,
+            label='Group'
+        )
+
         if GroupAccount.objects.filter(id=group_account_id).count():
             self.fields['group_account'].initial = GroupAccount.objects.get(id=group_account_id)
         # self.fields['modifications'] = forms.ModelMultipleChoiceField(queryset=Modification.objects.all(),
         #                                                               required=False,
         #                                                               widget=forms.MultipleHiddenInput())
-        self.fields['date'] = forms.DateTimeField(widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}),
-                                                  initial=datetime.now)
+        self.fields['date'] = forms.DateTimeField(
+            widget=DateTimePicker(options={"format": "YYYY-MM-DD", "pickTime": False}),
+            initial=datetime.now
+        )
 
     class Meta:
         model = TransactionReal
