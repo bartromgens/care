fe8514b4d6ff7ffabd9b71ee41cad43a33ee9ed2
Bart Römgens <bart.romgens@gmail.com>
2017-08-16 16:21:34 +0200

    move database settings to local settings #16
    Also cleanup of settings.py

--- care/local_settings_example.py
+++ care/local_settings_example.py
@@ -1,9 +1,23 @@
 # user settings, included in settings.py
 
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
 DEBUG = True
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = ''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(BASE_DIR, 'care.sqlite'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
 
 ADMINS = (
     ('Your Name', 'email address'),
